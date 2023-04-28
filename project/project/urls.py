f2fca89503e1d8b1fb0d82833733fff9ccfabe4c
dodu204 <dodu204@gmail.com>
2023-04-28 17:48:56 +0300

    Test

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('pages/', include('django.contrib.flatpages.urls')),
-   path('products/', include('simpleapp.urls')),
+    path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('products/', include('simpleapp.urls')),
 ]
